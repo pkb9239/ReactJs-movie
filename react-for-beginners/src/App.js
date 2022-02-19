6b9b42991b36a11e31e67e5b2b21e1e4241ee4a6
Kyungbin <pkb8839@naver.com>
2022-02-19 23:44:47 +0900

    [React] cleanup 코드 사용법

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,28 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
- 
-  console.log('i run all the time');
+function Hello() {
   useEffect(() => {
-    console.log('i run only once');
-  }, []); 
-  useEffect(() => {
-    console.log("i run when 'keyword' changes.");
-  }, [keyword]); 
-  useEffect(() => {
-    console.log("i run when 'counter' changes.");
-  }, [counter]); 
+    console.log('created :)');
+    return () => console.log('bye :(');
+  }, [])
+  return <h1>Hello</h1>
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-    <input value={keyword} onChange={onChange} type="text" placeholder="Search here.."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
