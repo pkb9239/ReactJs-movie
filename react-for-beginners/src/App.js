671cc3eff534c60f069650619487715abe51a9f8
Kyungbin <pkb8839@naver.com>
2022-02-19 23:23:29 +0900

    [React] useEffect 공부

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,28 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+ 
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log('i run only once');
+  }, []); 
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]); 
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]); 
+
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={'Continue'} />
+    <input value={keyword} onChange={onChange} type="text" placeholder="Search here.."></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+
     </div>
   );
 }
